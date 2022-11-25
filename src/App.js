49b6cde3bccaa09cd074c93587a216e7daf20e00
Dennis M Henderson <dennismhenderson@denniss-mbp.lan>
2022-11-25 13:52:15 -0500

    updated React index page text and dockerhub account used in deploy.yaml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is React Again!
+          This is Dennis React Application
         </p>
         <a
           className="App-link"
